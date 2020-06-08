10ceef4697cc78615a52007f425ba5140f85f25a
Pasha Khodak <57356490+w00t22w00t@users.noreply.github.com>
2020-06-09 01:47:45 +0300

    Update main.js

--- TimeLine/js/main.js
+++ TimeLine/js/main.js
@@ -1,71 +1,66 @@
-const angles = [38, 28, 18, 8, 0, -8, -18, -28, -38];
-
-const timeLine = document.querySelector('.time-line'),
-dots = document.querySelectorAll('.time-line .rotate'),
-descWindow = document.querySelector('.desc-block'),
-windowYear = document.querySelector('.block-year'),
-line = document.querySelector('.line');
-
-let beforeItem = 0;
-let count = 4;
-var startx = 0;
-var dist = 0;
-
-dots.forEach(function(item, index) {
-	item.addEventListener('click', function(){
-		currentYear(this);
-
-		timeLine.style.transform = 'rotate(' + angles[index] + 'deg)';
-		descWindow.style.top = -index * 60 + 'px';
-		windowYear.style.top = -index * 186 + 'px';
-		line.style.backgroundPosition = -index * 16 + 'px';
+(function(){
+	const angles = [38, 28, 18, 8, 0, -8, -18, -28, -38];
+	
+	const timeLine = document.querySelector('.time-line'),
+		  dots = document.querySelectorAll('.time-line .rotate'),
+		  descWindow = document.querySelector('.desc-block'),
+		  windowYear = document.querySelector('.block-year'),
+		  line = document.querySelector('.line');
+	
+	let counter = 4;
+	
+	dots.forEach(function(item, index) {
+		item.addEventListener('click', function(){
+			currentYear(this, index);		
+		});
 	});
-});
-
-if (timeLine.addEventListener) {
-	if ('onwheel' in document) {
-	    // IE9+, FF17+, Ch31+
-	    timeLine.addEventListener("wheel", onWheel);
-	} else if ('onmousewheel' in document) {
-	    // устаревший вариант события
-	    timeLine.addEventListener("mousewheel", onWheel);
-	} else {
-	    // Firefox < 17
-	    timeLine.addEventListener("MozMousePixelScroll", onWheel);
-	}
-} else { // IE8-
-	timeLine.attachEvent("onmousewheel", onWheel);
-}
-
-function onWheel(e) {
-	e = e || window.event;
-	e.preventDefault ? e.preventDefault() : (e.returnValue = false);
-
-	var delta = e.deltaY || e.detail || e.wheelDelta;
-
-	if (count >= 0 && count <= 8) {
-		count = count + delta/100;
-
-		if( count > 8) {
-			count = 8;
-		}
-		else if( count < 0) {
-			count = 0;
-		}
-	}
-	currentYear(dots[count]);
-	windowYear.style.top = -count * 186 + 'px';
-	descWindow.style.top = -count * 60 + 'px';
-	timeLine.style.transform = 'rotate(' + angles[count] + 'deg)';
-	line.style.backgroundPosition = -count * 16 + 'px';
-}
-
-function currentYear(current){
-	for (let i = 0; i < dots.length; i++) {
-		if (dots[i] === current) {
-			dots[i].classList.add('active-year');
+	
+	if (timeLine.addEventListener) {
+		if ('onwheel' in document) {
+		    // IE9+, FF17+, Ch31+
+		    timeLine.addEventListener("wheel", onWheel);
+		} else if ('onmousewheel' in document) {
+		    // устаревший вариант события
+		    timeLine.addEventListener("mousewheel", onWheel);
 		} else {
-			dots[i].classList.contains('active-year') && dots[i].classList.remove('active-year');
+		    // Firefox < 17
+		    timeLine.addEventListener("MozMousePixelScroll", onWheel);
 		}
+	} else { // IE8-
+		timeLine.attachEvent("onmousewheel", onWheel);
 	}
-}
+	
+	function onWheel(e) {
+		e = e || window.event;
+		e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+	
+		var delta = e.deltaY || e.detail || e.wheelDelta;
+	
+		if (counter >= 0 && counter <= 8) {
+			counter = counter + delta/100;
+	
+			if( counter > 8) {
+				counter = 8;
+			}
+			else if( counter < 0) {
+				counter = 0;
+			}
+		}
+		currentYear(dots[counter], counter);
+	}
+	
+	function currentYear(current, dot){
+		for (let i = 0; i < dots.length; i++) {
+			if (dots[i] === current) {
+				dots[i].classList.add('active-year');
+			} else {
+				dots[i].classList.contains('active-year') && dots[i].classList.remove('active-year');
+			}
+		}
+	
+		timeLine.style.transform = 'rotate(' + angles[dot] + 'deg)';
+		descWindow.style.top = -dot * 60 + 'px';
+		windowYear.style.top = -dot * 186 + 'px';
+		line.style.backgroundPosition = -dot * 16 + 'px';
+	}
+})();
